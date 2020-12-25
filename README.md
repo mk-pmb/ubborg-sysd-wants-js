c1f1962d17752655566d147c3e85ff29ddfb2bbb
mk-pmb <mk-pmb@users.noreply.github.com>
2020-12-25 06:46:42 +0100

    v0.0.2: Add mask function

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ ubborg-sysd-wants
 
 <!--#echo json="package.json" key="description" -->
 Plan a symlink to express a systemd &quot;wants&quot; relation between two
-units.
+units, and/or (un)mask a unit.
 <!--/#echo -->
 
 
@@ -14,20 +14,36 @@ units.
 API
 ---
 
-This module exports one function:
+This module exports one function that holds another function:
 
 ### sysdWants(who, wants, what)
 
-* `who` (string): Unit ID of the systemd trigger unit,
+* `who` (string¹): Unit ID of the systemd trigger unit,
   e.g. `'local-fs.target'`.
 * `wants` (boolean): Whether `who` "wants" `what`.
-  `true` = request the symlink exists,
+  `true` = request the symlink exists and points to `what`,
   `false` = request its absence.
-* `what` (object): `file` spec of the systemd unit that shall be triggered.
+* `what` (object¹): `file` spec of the systemd unit that shall be triggered.
+
+
+
+### .mask(unitName, masked)
+
+* `unitName` (string¹): The name of the symlink to be created or removed in
+  `/etc/systemd/system/`.
+* `masked` (boolean): Whether the unit shall be masked.
+  `true` = request the symlink exists and points to `'/dev/null'`,
+  `false` = request its absence.
 
 
 
 <!--#toc stop="scan" -->
+
+-----
+
+¹)  May also be an array, in which case an array will be returned with the
+    results of each possible combination with the other argument(s).
+
 
 
 
--- wants.mjs
+++ wants.mjs
@@ -3,20 +3,58 @@
 import pathLib from 'path';
 
 
-function fullPath(f) { return (f.pathPre || '') + f.path + (f.pathSuf || ''); }
+const isAry = Array.isArray;
+const sds = '/systemd/system/';
+
+
+function flatMap(l, f) { return [].concat(...l.map(f)); }
+
+
+function fullPath(f) {
+  if ((!f.path) && f.substr) { return '/lib' + sds + f; }
+  return (f.pathPre || '') + f.path + (f.pathSuf || '');
+}
+
+
+function mustBool(val, optName) {
+  if (typeof val === 'boolean') { return val; }
+  throw new TypeError('Unsupported value for "' + optName + '" param');
+}
+
+
+function maybeSym(d) {
+  return { mimeType: (d ? 'sym' : null), ...(d && { content: d }) };
+}
 
 
 function sysdWants(who, wants, what) {
+  if (isAry(who)) { return flatMap(who, u => sysdWants(u, wants, what)); }
+  if (isAry(what)) { return flatMap(what, u => sysdWants(who, wants, u)); }
   const dest = fullPath(what);
-  const sym = {
-    pathPre: '/etc/systemd/system/',
+  return {
+    pathPre: '/etc' + sds,
     path: who,
     pathSuf: '.wants/' + pathLib.basename(dest),
-    mimeType: null,
-    ...(wants && { mimeType: 'sym', content: dest }),
+    ...maybeSym(mustBool(wants, 'wants') && dest),
   };
-  return sym;
 }
+
+
+function mask(unitName, masked) {
+  if (Array.isArray(unitName)) { return unitName.map(u => mask(u, masked)); }
+  return {
+    path: '/etc' + sds + unitName,
+    ...maybeSym(mustBool(masked, 'masked') && '/dev/null'),
+  };
+}
+
+
+
+Object.assign(sysdWants, {
+
+  mask,
+
+});
 
 
 export default sysdWants;
